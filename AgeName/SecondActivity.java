c6809f78f98c849b985fb8157ac71e7052c68c0b
MAYANK SHEKHAR <mmayanksshekhar@gmail.com>
2019-09-26 08:45:03 -0700

    Update SecondActivity.java

--- AgeName/SecondActivity.java
+++ AgeName/SecondActivity.java
@@ -11,10 +11,15 @@ public class SecondActivity extends AppCompatActivity{
         setContentView(R.layout.secactivity);
         txtName = (TextView) findViewById(R.id.textView3);
         Intent intename = getIntent();
+        String out="",outp="";
         String g = (String) intename.getSerializableExtra("GENDER");
         String n = (String) intename.getSerializableExtra("NAME");
         String a = (String) intename.getSerializableExtra("AGE");
-        n=n.toUpperCase();
-        txtName.setText("Welcome " + n +", \nYou are :"+g+" \nYour AGE is:- "+a);
+        String[] name=n.split(" ");
+        for(int i=0;i<name.length-1;i++)
+            out=out+name[i].charAt(0)+" ";
+        if(g.equals("MALE")) outp="Mr. "+out.trim().toUpperCase()+" "+name[name.length-1].toUpperCase();
+        else outp="Mrs. "+out.trim().toUpperCase()+" "+name[name.length-1].toUpperCase();
+        txtName.setText("Welcome " + outp +", \nYou are :"+g+" \nYour AGE is:- "+a);
     }
 }
